bf2425c67ed33ed489f4667a736b22cb7beb1ee6
Kartik <94182623+kartik3805@users.noreply.github.com>
2021-11-18 20:08:23 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -856,17 +856,5 @@ gradientFill.addColorStop(0, "rgba(247, 180, 44, 0.8)");
 
 /* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js');
-}
-
-/**
- * Warn the page must be served over HTTPS
- * The `beforeinstallprompt` event won't fire if the page is served over HTTP.
- * Installability requires a service worker with a fetch event handler, and
- * if the page isn't served over HTTPS, the service worker won't load.
- */
-if (window.location.protocol === 'http:') {
-  const link = requireHTTPS.querySelector('a');
-  link.href = window.location.href.replace('http://', 'https://');
- console.log(`This page must served over a secure https`)
+  navigator.serviceWorker.register('js/service-worker.js');
 }
