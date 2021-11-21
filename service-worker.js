a4a55c9a986e28273f2e224c6adf87cd34b60241
Kartik <kartik22455@gmail.com>
2021-11-21 17:01:27 +0530

    Bug fixes and improvements

--- service-worker.js
+++ service-worker.js
@@ -30,5 +30,23 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', function(event) {
   // console.log('[Service Worker] Fetch', event.request.url);
-  
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        console.log('[Service Worker] Fetch failed; returning offline page instead.', error);
+
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+        return cachedResponse;
+      }
+    })());
+  }
 });
